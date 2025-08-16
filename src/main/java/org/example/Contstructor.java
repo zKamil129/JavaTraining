4c61e5c27a281cafb77679ca06d54cd2204875bd
Kamil Zola <kzola@amusys.pl>
2025-08-16 17:54:13 +0200

    init commit

---
+++ src/main/java/org/example/Contstructor.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Contstructor {
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog("Hestia", 4 );
+        System.out.println(myDog.name + " " + myDog.age);
+
+
+
+    }
+}
---
+++ src/main/java/org/example/Dog.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Dog {
+    String name;
+    int age;
+
+    public Dog(){
+
+    }
+
+    public Dog(String name){
+        this.name = name;
+    }
+
+    public Dog(int age){
+        this.age = age;
+    }
+
+    public Dog(String name, int age){
+        this.age = age;
+        this.name = name;
+    }
+}
